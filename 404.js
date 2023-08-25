ac2b2df89b6a8ffc27fe219ff83041ecb972d0aa
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-08-25 18:07:32 +0630

    Create 404.js

---
+++ 404.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script>
+        setTimeout(function() {
+            window.location.href = "/";
+        }, 3000); // Redirect after 3 seconds
+    </script>
+</head>
+<body>
+    <h1>Page Not Found</h1>
+    <p>This page doesn't exist. You will be redirected to the homepage shortly.</p>
+</body>
+</html>
