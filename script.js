1e42202787324b11e019ad329bb7ffc46f1de2a8
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-10-08 11:55:25 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function openChannel() {
+  window.location.href = 'https://t.me/drivetalkchannel';
+}
+function openGroup() {
+  window.location.href = 'https://t.me/drivetalk';
+}
+
+// Function for the "Privacy" button
+function openPrivacy() {
+  alert('Privacy button clicked');
+}
