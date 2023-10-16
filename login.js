4ad8a79cb145a7f26ee82763c655bb8435568e08
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-10-16 21:03:29 +0630

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-function onload() {
-    const PARAMS = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-    });
-    const GURl = PARAMS.gurl;
-    document.querySelector('.gurl').textContent = GURL;
