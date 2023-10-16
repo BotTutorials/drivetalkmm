512d26164148e30f34eb02cef9710fc19df6df83
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-10-16 20:33:49 +0630

    Update login.js

--- login.js
+++ login.js
@@ -2,5 +2,5 @@ function onload() {
     const PARAMS = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
     });
-    const URl = PARAMS.url;
-    document.querySelector('.url').textContent = URL;
+    const GURl = PARAMS.url;
+    document.querySelector('.gurl').textContent = GURL;
