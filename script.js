51ba77755c6626b93328e7100201b1c51af34c19
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-10-17 06:05:02 +0630

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ function Group() {
 function Privacy() {
   window.location.href = '/privacy';
 }
+function Login() {
+  window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=199463681728-8jl16j5urv86g6n7poqj85b02v6irk1e.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.drivetalkmm.tech%2Fgdrive-auth&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&access_type=offline&response_type=code&prompt=consent';
+}
